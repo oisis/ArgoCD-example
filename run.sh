74e07d77c28de08e0c384e2e562074da711555c8
OiSiS <oisisk@gmail.com>
2024-12-01 19:40:13 +0100

    Add colors to run.sh outputs

--- run.sh
+++ run.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-echo "Adding ArgoCD Helm repo"
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+printf "${GREEN}Adding ArgoCD Helm repo${NC}\n"
 helm repo add argocd https://argoproj.github.io/argo-helm
 
-echo "Installing ArgoCD with Helm"
+printf "${GREEN}Installing ArgoCD with Helm${NC}\n"
 helm install argocd argocd/argo-cd --version 7.7.6 \
   -f ./argocd-bootstrap/helm/argocd-values.yaml \
   --create-namespace \
   -n argocd
 
-echo "Applying K8s manifests to finish ArgoCD bootstrap"
+printf "${GREEN}Applying K8s manifests to finish ArgoCD bootstrap${NC}\n"
 kubectl apply -f ./argocd-bootstrap/manifests/
 
-echo "Getting ArgoCD admin password"
-argocd admin initial-password -n argocd
+ARGOPASS=$(argocd admin initial-password -n argocd | head -n 1)
+printf "${GREEN}ArgoCD admin password${NC}: ${RED}${ARGOPASS}${NC}\n"
 
-echo "To get access to ArgoCD UI run command:"
-echo "    kubectl port-forward svc/argocd-server -n argocd 8080:443"
-echo "Open http://localhost:8080 in webbrowser"
+printf "${GREEN}To get access to ArgoCD UI run command:${NC}\n"
+printf "    kubectl port-forward svc/argocd-server -n argocd 8080:443$\n"
+printf "${GREEN}Open http://localhost:8080 in webbrowser${NC}\n"
